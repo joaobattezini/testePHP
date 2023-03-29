f6a3d6626decda53553b3ebeec110da1b5614009
João Pedro Battezini <joaobattezini@gmail.com>
2023-03-29 09:05:16 -0300

    alterado o nome das paginas

---
+++ public/views/teste.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>Pedidos</title>
+    <link rel="apple-touch-icon" href="../../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="../../app-assets/vendors/css/vendors.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/components.css">
+    <!-- END: Theme CSS-->
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/core/menu/menu-types/horizontal-menu.css">
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="../../app-assets/css/pages/user-feed.css">
+    <!-- END: Page CSS-->
+
+</head>
+
+<body>
+
+
+    <div class="card shadow-none mt-2">
+        <div class="card-body">
+            <div class="twitter-feed">
+                <h5 class="card-title mb-1">Pedidos</h5>
+                <hr>
+                <div class="row mt-3">
+                    <div class="col-3">
+                        <img src="../../app-assets/images/icons/twitter-avatar1.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-9 font-small-3 pl-0">
+                        <p class="m-0">Lorem ipsum dolor sit amet, consectetuer elit</p>
+                        <p class="m-0">Local : </p>
+                    </div>
+                    <div class="col-12 font-small-3 mt-1">
+                        <p class="primary m-0">Cliente</p>
+                        <p>2 hours ago</p>
+                    </div>
+                </div>
+                <hr class="m-0">
+                <div class="row mt-3">
+                    <div class="col-3">
+                        <img src="../../app-assets/images/icons/twitter-avatar1.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-9 font-small-3 pl-0">
+                        <p class="m-0">Lorem ipsum dolor sit amet, consectetuer elit</p>
+                        <p class="m-0">Local :</p>
+                    </div>
+                    <div class="col-12 font-small-3 mt-1">
+                        <p class="primary m-0">Cliente</p>
+                        <p>2 hours ago</p>
+                    </div>
+                </div>
+                <hr class="m-0">
+                <div class="row mt-3">
+                    <div class="col-3">
+                        <img src="../../app-assets/images/icons/twitter-avatar1.png" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-9 font-small-3 pl-0">
+                        <p class="m-0">Lorem ipsum dolor sit amet, consectetuer elit</p>
+                        <p class="m-0">Local :</p>
+                    </div>
+                    <div class="col-12 font-small-3 mt-1">
+                        <p class="primary m-0">Cliente</p>
+                        <p>2 hours ago</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- BEGIN: Footer-->
+    <footer class="footer footer-transparent footer-light navbar-shadow">
+        <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2 container center-layout"><span class="float-md-left d-block d-md-inline-block">Copyright &copy; 2019 <a class="text-bold-800 grey darken-2" href="https://1.envato.market/modern_admin" target="_blank">PIXINVENT</a></span><span class="float-md-right d-none d-lg-block">Hand-crafted & Made with<i class="ft-heart pink"></i><span id="scroll-top"></span></span></p>
+    </footer>
+    <!-- END: Footer-->
+    
+    <script src="https://code.jquery.com/jquery-3.6.4.js" ></script>
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="../../app-assets/vendors/js/vendors.min.js"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="../../app-assets/vendors/js/ui/jquery.sticky.js"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="../../app-assets/js/core/app-menu.js"></script>
+    <script src="../../app-assets/js/core/app.js"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <!-- END: Page JS-->
+
+</body>
